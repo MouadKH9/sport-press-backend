37d63bb8862fc8fff6222d2fce8f8add6df85575
MouadK <cream@colorelephant.com>
2021-02-28 11:22:09 +0100

    Added sort and pagination

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use Illuminate\Support\Facades\DB;
 
 use App\Models\Article;
@@ -8,25 +9,28 @@ use Illuminate\Http\Request;
 
 class ArticleController extends Controller
 {
-    public function home()
+    public function home(Request $request)
     {
-        $all_articles = Article::all();
+        $sort = $request->get("sort", "id");
+        $dir = $request->get("dir", "desc");
+        $all_articles = Article::with('views')->orderBy($sort, $dir)->paginate(2);
         $top_articles = Article::with('views')->get()->sort(function ($a, $b) {
             return count($b->views()->get()) - count($a->views()->get());
         });
         return view('home', ['articles' => $all_articles, 'top_articles' => $top_articles]);
     }
+
+
     public function articleDetail($id)
     {
         //details of article
-        $article =DB::table('articles')->where('id', $id)->get();
+        $article = DB::table('articles')->where('id', $id)->get();
         //suggestion
-        $articles =DB::table('articles')->where('category_id', $article[0]->category_id)->where('id', '!=', $id)->get();
+        $articles = DB::table('articles')->where('category_id', $article[0]->category_id)->where('id', '!=', $id)->get();
 
-        return view('articleDetail',[
+        return view('articleDetail', [
             'article' => $article,
             'articles' => $articles
-            ]);
-
+        ]);
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,19 +6,29 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sport Press</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 
 <body>
-    <h3>
+    <h1 class="mb-5 text-xl	">
         All Articles:
-    </h3>
+    </h1>
+    <div>
+        Trier par:
+        <a href="?page={{$articles->currentPage()}}&sort=id&dir=DESC">Date</a>
+        <a href="?page={{$articles->currentPage()}}&sort=title&dir=ASC">AZ</a>
+        <a href="?page={{$articles->currentPage()}}&sort=views&dir=DESC">Popularite</a>
+    </div>
     @foreach ($articles as $article)
     <p>{{ $article->title }}</p>
     @endforeach
 
-    <h3>
+    {{ $articles->appends(request()->input())->links() }}
+
+
+    <h1 class="my-5 text-xl">
         Top 5 articles
-    </h3>
+    </h1>
     @foreach ($top_articles as $article)
     <p>{{ $article->title }}</p>
     @endforeach
