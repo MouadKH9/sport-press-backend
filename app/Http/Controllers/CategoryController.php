a00f58ef8c994dab1390921c148b90b179e58c14
AymaneBOUJELLABA <aymanoosse.b.j@gmail.com>
2021-02-28 03:28:49 +0100

    added category page

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -3,8 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Category;
+use App\Models\Article;
+use Illuminate\Support\Facades\DB;
 
 class CategoryController extends Controller
 {
-    //
+    public function categoryPage($name)
+    {
+        $category = Category::where('name',$name)->first();
+
+        //$articles = $category->articles();
+        
+        $articles =DB::table('articles')->where('category_id', $category->id)->get();
+
+        return view('categoryArticles',['category' => $category ,'articles' => $articles]);
+    }
 }
